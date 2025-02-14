3b8d80830ab13a61421149db2bf01df06de0cecf
MarianaAtehortua03 <marianaateh2005@gmail.com>
2025-02-14 01:22:53 +0000

    Ejercicios Clase2

---
+++ clase2/Bucles.java
@@ -0,0 +1,26 @@
+public class Bucles {
+
+    public static void main(String[] args) {
+
+        // for(inicio; condicion de parada; incremento o decremento)
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Numero de Iteracion: " + i);
+        }
+
+        /* Estructura del while
+         * 
+         * incio;
+         * while(condicion){
+         * ----------
+         * incremento o decremento
+         * }
+         * 
+         */
+
+         int j =0;
+         while(j <= 5) {
+            System.out.println("Numero de Iteracion: " + j);
+            j++;
+         }
+    }
+}

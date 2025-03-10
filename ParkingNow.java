a29fde03f29302756a5b2e09529ff8b90a52ff3f
MarianaAtehortua03 <marianaateh2005@gmail.com>
2025-03-10 14:22:39 +0000

    funcion añadida para registrar motos en puestos disponibles

--- ParkingNow.java
+++ ParkingNow.java
@@ -2,18 +2,38 @@ public class ParkingNow {
     // 0 = vacío, 1 = ocupado
     static int[] parqueaderoBajo = new int[20];
     static int[] parqueaderoAlto = new int[10];
+
     public static void main(String[] args) {
         System.out.println("Estado inicial del parqueadero:");
         mostrarDisponibilidad();
 
-        // simulacin de registro de una moto en bajo cilindraje (puesto 0)
-        parqueaderoBajo[0] = 1;
-
-        // simulacion de registro de una moto en alto cilindraje (puesto 0)
-        parqueaderoAlto[0] = 1;
+        // Simulacion de registro de motos
+        registrarMoto(300); // Moto de bajo cilindraje
+        registrarMoto(600); // Moto de alto cilindraje
 
         System.out.println("\nDespués de registrar 2 motos:");
         mostrarDisponibilidad();
+    }
+
+    public static void registrarMoto(int cilindraje) {
+        if (cilindraje < 400) { // Motos de bajo cilindraje
+            for (int i = 0; i < parqueaderoBajo.length; i++) {
+                if (parqueaderoBajo[i] == 0) { // Encuentra un puesto vacio
+                    parqueaderoBajo[i] = 1; // Lo ocupa
+                    System.out.println("Moto registrada en puesto " + (i + 1) + " (bajo cilindraje).");
+                    return;
+                }
+            }
+        } else { // Motos de alto cilindraje
+            for (int i = 0; i < parqueaderoAlto.length; i++) {
+                if (parqueaderoAlto[i] == 0) { // Encuentra un puesto vacio
+                    parqueaderoAlto[i] = 1; // Lo ocupa
+                    System.out.println("Moto registrada en puesto " + (i + 1) + " (alto cilindraje).");
+                    return;
+                }
+            }
+        }
+        System.out.println("No hay puestos disponibles para este tipo de moto");
     }
 
     public static void mostrarDisponibilidad() {

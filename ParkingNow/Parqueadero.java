aad19ff76462deb3edd2efc4b30323f143c203c2
MarianaAtehortua03 <marianaateh2005@gmail.com>
2025-03-12 02:44:17 +0000

    Se agrego la estructura base de la clase Parqueadero

--- ParkingNow/Parqueadero.java
+++ ParkingNow/Parqueadero.java
@@ -1,5 +1,13 @@
 package ParkingNow;
 
+import java.util.Scanner;
+
 public class Parqueadero {
-    
+    private Moto[] bajoCilindraje;
+    private Moto[] altoCilindraje;
+    private Scanner scanner;
+
+    public Parqueadero() {
+        // 
+    }
 }
